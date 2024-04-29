8f3527908ca555e4d378e4b5a9f3cd50c6d8b99f
Yan_ice <2364728692@qq.com>
2024-04-29 14:37:56 +0800

    fix error (0.1.11)

--- src/nkapi.rs
+++ src/nkapi.rs
@@ -8,25 +8,25 @@ use super::config::NK_TRAMPOLINE;
 /// the value below is NK call number.
 /// 
 
-pub const NKAPI_TRAP_HANDLE: usize = 0;
-pub const NKAPI_CONFIG: usize = 1;
-pub const NKAPI_PT_INIT: usize = 2;
-pub const NKAPI_PT_DESTROY: usize = 3;
-pub const NKAPI_ALLOC: usize = 4;
-pub const NKAPI_DEALLOC: usize = 5;
-pub const NKAPI_ACTIVATE: usize = 6;
-pub const NKAPI_TRANSLATE: usize = 7;
-pub const NKAPI_SET_PERM: usize = 8;
-pub const NKAPI_GET_PTE: usize = 9;
-pub const NKAPI_WRITE: usize = 10;
-pub const NKAPI_FORK_PTE: usize = 11;
-pub const NKAPI_TIME: usize = 12;
-pub const NKAPI_DEBUG: usize = 13;
+pub const MMKAPI_TRAP_HANDLE: usize = 0;
+pub const MMKAPI_CONFIG: usize = 1;
+pub const MMKAPI_PT_INIT: usize = 2;
+pub const MMKAPI_PT_DESTROY: usize = 3;
+pub const MMKAPI_ALLOC: usize = 4;
+pub const MMKAPI_DEALLOC: usize = 5;
+pub const MMKAPI_ACTIVATE: usize = 6;
+pub const MMKAPI_TRANSLATE: usize = 7;
+pub const MMKAPI_SET_PERM: usize = 8;
+pub const MMKAPI_GET_PTE: usize = 9;
+pub const MMKAPI_WRITE: usize = 10;
+pub const MMKAPI_FORK_PTE: usize = 11;
+pub const MMKAPI_TIME: usize = 12;
+pub const MMKAPI_DEBUG: usize = 13;
 
-pub const MMKAPI_ID_MEMBLOCK_SET_RANGE = 14;
-pub const MMKAPI_ID_MEMBLOCK_ALLOC_RANGE = 15;
-pub const MMKAPI_ID_MEMBLOCK_SET_FLAG = 16;
-pub const MMKAPI_ID_INQUIRE_MEMBLOCK = 17;
+pub const MMKAPI_MEMBLOCK_SET_RANGE: usize = 14;
+pub const MMKAPI_MEMBLOCK_ALLOC_RANGE: usize = 15;
+pub const MMKAPI_MEMBLOCK_SET_FLAG: usize = 16;
+pub const MMKAPI_INQUIRE_MEMBLOCK: usize = 17;
 
 ///
 ///////////////////////////////////
@@ -37,13 +37,13 @@ pub const MMKAPI_ID_INQUIRE_MEMBLOCK = 17;
 /// 
 
 
-pub const NKCFG_S_DELEGATE: usize = 0;
-pub const NKCFG_U_DELEGATE: usize = 1; 
-pub const NKCFG_SIGNAL: usize = 2;
-pub const NKCFG_ALLOCATOR_START: usize = 3;
-pub const NKCFG_ALLOCATOR_END: usize = 4;
+pub const MMKCFG_S_DELEGATE: usize = 0;
+pub const MMKCFG_U_DELEGATE: usize = 1; 
+pub const MMKCFG_SIGNAL: usize = 2;
+pub const MMKCFG_ALLOCATOR_START: usize = 3;
+pub const MMKCFG_ALLOCATOR_END: usize = 4;
 
-pub const MMKCFG_ID_MIN_PFN: usize = 5;
+pub const MMKCFG_MIN_PFN: usize = 5;
 
 ///
 ///////////////////////////////////
