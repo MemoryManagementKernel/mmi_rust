86f086a742507f52bfeb3db5b3880a31523f613a
Yan_ice <2364728692@qq.com>
2024-04-29 14:40:58 +0800

    fix error ()0.1.12

--- src/nkapi.rs
+++ src/nkapi.rs
@@ -145,7 +145,7 @@ macro_rules! entry_gate {
 pub fn nkapi_time() -> usize{
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_TIME, retval0, retval1);
+    entry_gate!(MMKAPI_TIME, retval0, retval1);
     if retval1 != 0 {
         panic!("Error occurs.");
     }
@@ -155,7 +155,7 @@ pub fn nkapi_time() -> usize{
 pub fn nkapi_current_pt() -> usize{
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_CURRENT_PT, retval0, retval1);
+    entry_gate!(MMKAPI_CURRENT_PT, retval0, retval1);
     if retval1 != 0 {
         panic!("Error occurs.");
     }
@@ -166,7 +166,7 @@ pub fn nkapi_current_pt() -> usize{
 pub fn nkapi_translate(pt_handle: usize, vpn:VirtPageNum, write: bool) -> Option<PhysPageNum>{
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_TRANSLATE,pt_handle,vpn,write, retval0, retval1);
+    entry_gate!(MMKAPI_TRANSLATE,pt_handle,vpn,write, retval0, retval1);
     if retval1 == 0{
         return Some(retval0.into());
     }
@@ -189,7 +189,7 @@ pub fn nkapi_get_pte(pt_handle: usize, vpn: VirtPageNum) -> Option<usize>{
     // None
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_GET_PTE,pt_handle,vpn, retval0, retval1);
+    entry_gate!(MMKAPI_GET_PTE,pt_handle,vpn, retval0, retval1);
     if retval1 == 0{
         return Some(retval0.into());
     }
@@ -200,7 +200,7 @@ pub fn nkapi_get_pte(pt_handle: usize, vpn: VirtPageNum) -> Option<usize>{
 pub fn nkapi_fork_pte(pt_handle: usize, pt_child: usize, vpn: VirtPageNum, cow: bool) -> Option<PhysPageNum> {
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_FORK_PTE, pt_handle, pt_child, vpn, cow, retval0, retval1);
+    entry_gate!(MMKAPI_FORK_PTE, pt_handle, pt_child, vpn, cow, retval0, retval1);
     if retval1 == 0{
         return Some(retval0.into());
     }
@@ -211,7 +211,7 @@ pub fn nkapi_fork_pte(pt_handle: usize, pt_child: usize, vpn: VirtPageNum, cow: 
 pub fn nkapi_alloc(pt_handle: usize, vpn: VirtPageNum, map_type: MapType, perm: MapPermission)-> PhysPageNum{
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_ALLOC, pt_handle, vpn, 1 as usize, usize::from(map_type), perm, 
+    entry_gate!(MMKAPI_ALLOC, pt_handle, vpn, 1 as usize, usize::from(map_type), perm, 
     retval0, retval1);
     return retval0.into();
 }
@@ -220,7 +220,7 @@ pub fn nkapi_alloc_mul(pt_handle: usize, vpn_start: VirtPageNum, vpn_end: VirtPa
     let retval0: usize;
     let retval1: usize;
     let size = vpn_end.0 - vpn_start.0 + 1;
-    entry_gate!(NKAPI_ALLOC, pt_handle, vpn_start, size, usize::from(map_type), perm, 
+    entry_gate!(MMKAPI_ALLOC, pt_handle, vpn_start, size, usize::from(map_type), perm, 
     retval0, retval1);
     return retval0.into();
 }
@@ -229,60 +229,60 @@ pub fn nkapi_pt_init(pt_handle: usize, regenerate: bool){
     let retval0: usize;
     let retval1: usize;
 
-    entry_gate!(NKAPI_PT_INIT,pt_handle, regenerate, retval0, retval1);
+    entry_gate!(MMKAPI_PT_INIT,pt_handle, regenerate, retval0, retval1);
 }
 
 pub fn nkapi_pt_destroy(pt_handle: usize){
     let retval0: usize;
     let retval1: usize;
 
-    entry_gate!(NKAPI_PT_DESTROY,pt_handle, retval0, retval1);
+    entry_gate!(MMKAPI_PT_DESTROY,pt_handle, retval0, retval1);
 }
 
 pub fn nkapi_dealloc(pt_handle: usize, vpn: VirtPageNum){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_DEALLOC, pt_handle, vpn,retval0, retval1);
+    entry_gate!(MMKAPI_DEALLOC, pt_handle, vpn,retval0, retval1);
 }
 
 pub fn nkapi_activate(pt_handle: usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_ACTIVATE, pt_handle ,retval0, retval1);
+    entry_gate!(MMKAPI_ACTIVATE, pt_handle ,retval0, retval1);
 }
 
 pub fn nkapi_write(pt_handle: usize, mut current_vpn: VirtPageNum, data: &[u8], len: usize, offset:usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_WRITE,pt_handle, current_vpn, 
+    entry_gate!(MMKAPI_WRITE,pt_handle, current_vpn, 
         data as *const [u8] as *const usize as usize, len, offset, retval0, retval1);
 }
 
 pub fn nkapi_set_user_delegate_handler(entry: usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_CONFIG, NKCFG_U_DELEGATE, entry,
+    entry_gate!(MMKAPI_CONFIG, NKCFG_U_DELEGATE, entry,
         retval0, retval1);
 }
 
 pub fn nkapi_set_kernel_delegate_handler(entry: usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_CONFIG, NKCFG_S_DELEGATE, entry,
+    entry_gate!(MMKAPI_CONFIG, NKCFG_S_DELEGATE, entry,
         retval0, retval1);
 }
 
 pub fn nkapi_set_signal_handler(entry: usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_CONFIG, NKCFG_SIGNAL, entry,
+    entry_gate!(MMKAPI_CONFIG, NKCFG_SIGNAL, entry,
         retval0, retval1);
 }
 
 pub fn nkapi_set_allocator_start(begin: usize){
     let mut retval0: usize;
     let mut retval1: usize;
-    entry_gate!(NKAPI_CONFIG, NKCFG_ALLOCATOR_START, begin,
+    entry_gate!(MMKAPI_CONFIG, NKCFG_ALLOCATOR_START, begin,
         retval0, retval1);
     if retval1 != 0 {
         panic!("Error occurs.");
@@ -292,7 +292,7 @@ pub fn nkapi_set_allocator_start(begin: usize){
 pub fn nkapi_set_allocator_end(end: usize){
     let mut retval0: usize;
     let mut retval1: usize;
-    entry_gate!(NKAPI_CONFIG, NKCFG_ALLOCATOR_END, end,
+    entry_gate!(MMKAPI_CONFIG, NKCFG_ALLOCATOR_END, end,
         retval0, retval1);
 }
 
@@ -300,13 +300,13 @@ pub fn nkapi_set_allocator_end(end: usize){
 pub fn nkapi_set_permission(pt_handle: usize, vpn:VirtPageNum, flags: usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_SET_PERM, pt_handle, vpn, flags,retval0, retval1);
+    entry_gate!(MMKAPI_SET_PERM, pt_handle, vpn, flags,retval0, retval1);
 }
 
 pub fn nkapi_print_pt(pt_handle: usize, from: usize, to: usize){
     let retval0: usize;
     let retval1: usize;
-    entry_gate!(NKAPI_DEBUG, pt_handle, from, to ,retval0, retval1);
+    entry_gate!(MMKAPI_DEBUG, pt_handle, from, to ,retval0, retval1);
 }
 
 
